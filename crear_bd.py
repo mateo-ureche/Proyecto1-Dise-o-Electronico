24926108772da98a236c665dc95d2b4567a286be
mateo-ureche <mateureche28@gmail.com>
2026-09-01 20:49:09 -0500

    Create crear_bd.py

---
+++ crear_bd.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+conexion = sqlite3.connect("ubicaciones.db")
+
+cursor = conexion.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS ubicaciones (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    latitud REAL NOT NULL,
+    longitud REAL NOT NULL,
+    fecha TEXT NOT NULL,
+    hora TEXT NOT NULL
+)
+""")
+
+conexion.commit()
+conexion.close()
+
+print("Base de datos creada correctamente.")
+
+
